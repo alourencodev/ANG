6a1d98ec3ae62f2c1258db32f62ec95ea712716a
André Lourenço <alourenco.dev@gmail.com>
2021-02-21 18:37:06 +0100

    Add StaticArray Iterator

--- Core/StaticArray.hpp
+++ Core/StaticArray.hpp
@@ -5,124 +5,132 @@
 #include "Meta.hpp"
 #include "Types.hpp"
 
-#include <iterator>
-
 template<typename t_type, size_t t_size>
 class StaticArray
 {
 public:
 	
-	class Iterator : public std::iterator<std::random_access_iterator_tag, t_type>
+	template<typename t_iterType>
+	class _Iterator
 	{
 	public:
-		_nodiscard constexpr const t_type *operator->() const { _checkIndexBounds("derreference"); return _ptr + _index; }
-		_nodiscard constexpr t_type *operator->() { _checkIndexBounds("derreference"); return _ptr + _index; }
+		using iterator_category = std::random_access_iterator_tag;
+		using difference_type = ptrdiff_t;
+		using value_type = t_type;
+		using pointer = t_type *;
+		using reference = t_type &;
 
-		_nodiscard constexpr const t_type &operator*() const { return *operator->(); }
-		_nodiscard constexpr t_type &operator*() { return *operator->(); }
+		_Iterator(t_iterType *ptr) : _ptr(ptr), _index(0) {}
+		_Iterator(t_iterType *ptr, size_t index) : _ptr(ptr), _index(index) {}
 
-		_nodiscard constexpr const t_type &operator[](ptrdiff_t offset) const { return *(*this + offset); }
-		_nodiscard constexpr t_type &operator[](ptrdiff_t offset) { return *(*this + offset); }
+		_nodiscard constexpr const t_iterType *operator->() const { _checkIndexBounds("derreference"); return _ptr + _index; }
+		_nodiscard constexpr t_iterType *operator->() { _checkIndexBounds("derreference"); return _ptr + _index; }
 
-		constexpr Iterator &operator++()
+		_nodiscard constexpr const t_iterType &operator*() const { return *operator->(); }
+		_nodiscard constexpr t_iterType &operator*() { return *operator->(); }
+
+		_nodiscard constexpr const t_iterType &operator[](ptrdiff_t offset) const { return *(*this + offset); }
+		_nodiscard constexpr t_iterType &operator[](ptrdiff_t offset) { return *(*this + offset); }
+
+		constexpr _Iterator &operator++()
 		{
 			_checkIndexBounds("increment");
 			++_index;
 			return *this;
 		}
 
-		constexpr Iterator operator++(int)
+		_nodiscard constexpr _Iterator operator++(int)
 		{
-			Iterator temp = *this;
+			_Iterator temp = *this;
 			++*this;
 			return temp;
 		}
 
-		constexpr Iterator &operator--()
+		constexpr _Iterator &operator--()
 		{
 			_checkIndexBounds("decrement");
 			--_index;
 			return *this;
 		}
 
-		constexpr Iterator operator--(int)
+		_nodiscard constexpr _Iterator operator--(int)
 		{
-			Iterator temp = *this;
+			_Iterator temp = *this;
 			--*this;
 			return temp;
 		}
 
-		constexpr Iterator &operator+=(ptrdiff_t offset)
+		constexpr _Iterator &operator+=(ptrdiff_t offset)
 		{
 			_checkOffset(offset);
 			_index += static_cast<size_t>(offset);
 			return *this;
 		}
 
-		_nodiscard constexpr Iterator operator+(ptrdiff_t offset) const
+		_nodiscard constexpr _Iterator operator+(ptrdiff_t offset) const
 		{
 			_checkOffset(offset);
 			return *this + offset;
 		}
 
-		constexpr Iterator &operator-=(ptrdiff_t offset)
+		constexpr _Iterator &operator-=(ptrdiff_t offset)
 		{
 			_checkOffset(-offset);
 			_index -= static_cast<size_t>(offset);
 			return *this;
 		}
 
-		_nodiscard constexpr Iterator operator-(ptrdiff_t offset) const
+		_nodiscard constexpr _Iterator operator-(ptrdiff_t offset) const
 		{
 			_checkOffset(-offset);
 			return *this - offset;
 		}
 
-		_nodiscard constexpr ptrdiff_t operator+(const Iterator &other) const
+		_nodiscard constexpr ptrdiff_t operator+(const _Iterator &other) const
 		{
 			_checkIteratorCompatibility(other);
 			_checkOffset(other._index);
 			return static_cast<ptrdiff_t>(_index + other._index);
 		}
 
-		_nodiscard constexpr ptrdiff_t operator-(const Iterator &other) const 
+		_nodiscard constexpr ptrdiff_t operator-(const _Iterator &other) const 
 		{
 			_checkIteratorCompatibility(other);
 			_checkOffset(-other._index);
 			return static_cast<ptrdiff_t>(_index - other._index);
 		}
 
-		_nodiscard constexpr bool operator==(const Iterator &other) const
+		_nodiscard constexpr bool operator==(const _Iterator &other) const
 		{
 			_checkIteratorCompatibility(other);
 			return _index == other._index;
 		}
 
-		_nodiscard constexpr bool operator!=(const Iterator &other) const
+		_nodiscard constexpr bool operator!=(const _Iterator &other) const
 		{
 			_checkIteratorCompatibility(other);
 			return _index != other._index;
 		}
 
-		_nodiscard constexpr bool operator<(const Iterator &other) const
+		_nodiscard constexpr bool operator<(const _Iterator &other) const
 		{
 			_checkIteratorCompatibility(other);
 			return _index < other._index;
 		}
 
-		_nodiscard constexpr bool operator>(const Iterator &other) const
+		_nodiscard constexpr bool operator>(const _Iterator &other) const
 		{
 			_checkIteratorCompatibility(other);
 			return _index > other._index;
 		}
 
-		_nodiscard constexpr bool operator<=(const Iterator &other) const
+		_nodiscard constexpr bool operator<=(const _Iterator &other) const
 		{
 			_checkIteratorCompatibility(other);
 			return _index <= other._index;
 		}
 
-		_nodiscard constexpr bool operator>=(const Iterator &other) const
+		_nodiscard constexpr bool operator>=(const _Iterator &other) const
 		{
 			_checkIteratorCompatibility(other);
 			return _index >= other._index;
@@ -144,19 +152,21 @@ public:
 			logAssertFatal(_index < t_size, "Trying to %s iterator out of the array's range.", operationName)
 		}
 
-		constexpr void _checkIteratorCompatibility(const Iterator other) const
+		constexpr void _checkIteratorCompatibility(const _Iterator other) const
 		{
 			logAssertFatal(_ptr == other._ptr, "Incompatible array iterators. Both iterators must belong to the same array.");
 		}
 
-		t_type *_ptr = nullptr;
+		t_iterType *_ptr = nullptr;
 		size_t _index = 0;
 	};
 
-	using ReverseIterator = std::reverse_iterator<Iterator>;
+	using Iterator = _Iterator<t_type>;
+	using ConstIterator = _Iterator<const t_type>;
 
 	StaticArray() = default;
-	
+	StaticArray(const t_type &value) { fill(value); }
+
 	template<typename t_a, typename ...t_others>
 	StaticArray(t_a first, t_others ...others)
 		: _data{std::forward<t_a>(first), std::forward<t_others>(others)...}
@@ -165,13 +175,15 @@ public:
 		static_assert(meta::areSame<t_a, t_others...>::value, "Trying to initialize array with elements of different types.");
 	}
 
-	_force_inline constexpr const t_type &operator[](u32 index) const 
+	StaticArray(const StaticArray &other) = delete;
+
+	_force_inline constexpr const t_type &operator[](size_t index) const 
 	{ 
 		logAssertFatal(index < t_size, "StaticArray out of bounds! You are trying to access index %d of an array of size %d", index, t_size);
 		return _data[index]; 
 	}
 
-	_force_inline constexpr t_type &operator[](u32 index) 
+	_force_inline constexpr t_type &operator[](size_t index) 
 	{ 
 		logAssertFatal(index < t_size, "StaticArray out of bounds! You are trying to access index %d of an array of size %d", index, t_size);
 		return _data[index]; 
@@ -182,17 +194,12 @@ public:
 	_force_inline explicit constexpr operator const t_type *() { return _data; }
 	_force_inline explicit constexpr operator t_type *() { return _data; }
 
-	constexpr void fill(const t_type &value) { std::fill_n(_data, t_size, value); }
+	constexpr void fill(const t_type &value) { std::fill_n(_data, t_size, value); } // TODO: Use memset?
 
-	_nodiscard constexpr const Iterator begin() const { return Iterator(_data, 0); }
 	_nodiscard constexpr Iterator begin() { return Iterator(_data, 0); }
-	_nodiscard constexpr const Iterator end() const { return Iterator(_data, t_size - 1); }
-	_nodiscard constexpr Iterator end() { return Iterator(_data, t_size - 1);}
-
-	_nodiscard constexpr const ReverseIterator rBegin() const { return ReverseIterator(end()); }
-	_nodiscard constexpr ReverseIterator rBegin() { return ReverseIterator(end()); }
-	_nodiscard constexpr const ReverseIterator rEnd() const { return ReverseIterator(begin()); }
-	_nodiscard constexpr ReverseIterator rEnd() { return ReverseIterator(begin()); }
+	_nodiscard constexpr const ConstIterator begin() const { return ConstIterator(_data, 0); }
+	_nodiscard constexpr Iterator end() { return Iterator(_data, t_size - 1); }
+	_nodiscard constexpr const ConstIterator end() const { return ConstIterator(_data, t_size - 1); }
 
 	_nodiscard constexpr t_type &front() { return _data[0]; }
 	_nodiscard constexpr const t_type &front() const { return _data[0]; }
--- Core_Tests/StaticArray_Tests.cpp
+++ Core_Tests/StaticArray_Tests.cpp
@@ -2,19 +2,80 @@
 
 #include <Core/StaticArray.hpp>
 
+
 TEST_CASE("StaticaArray Constructors")
 {
 	// Default Constructor
-	StaticArray<int, 2> a1;
-	a1 = {0, 0}; //Surpress warning
+	SECTION ("Default Cnstructor")
+	{
+		StaticArray<i32, 2> a1;
+		a1 = {0, 0}; //Surpress warning
+	}
 
 	SECTION("Initialize from list")
 	{
-		StaticArray<int, 4> a2 = {1, 2, 3, 4};
+		StaticArray<i32, 3> a2 = {1, 2, 3};
 
 		REQUIRE(a2[0] == 1);
 		REQUIRE(a2[1] == 2);
 		REQUIRE(a2[2] == 3);
-		REQUIRE(a2[3] == 4);
 	}
+
+	SECTION("Initialize with filling value")
+	{
+		StaticArray <i32, 3> a3(100);
+
+		REQUIRE(a3[0] == 100);
+		REQUIRE(a3[1] == 100);
+		REQUIRE(a3[2] == 100);
+	}
+}
+
+TEST_CASE("StaticArray Iteration")
+{
+	StaticArray<i32, 4> testArray = {0, 1, 2, 3};
+
+	SECTION("Iterate by index")
+	{
+		for (i32 i = 0; i < testArray.size; i++)
+			REQUIRE(testArray[i] == i);
+	}
+
+	SECTION("Iterate for each")
+	{
+		i32 counter = 0;
+		for(i32 value : testArray)
+			REQUIRE(value == counter++);
+	}
+
+	// TODO: Iterate backwards
+}
+
+TEST_CASE("StaticArray Iterators")
+{
+	StaticArray<i32, 4> testArray = {0, 1, 2, 3};
+
+	REQUIRE(*testArray.begin() == 0);
+	REQUIRE(*testArray.end() == 3);
+
+	const StaticArray<i32, 4> constTestArray = {0, 1, 2, 3};
+
+	REQUIRE(*constTestArray.begin() == 0);
+	REQUIRE(*constTestArray.end() == 3);
+}
+
+TEST_CASE("StaticArray Access")
+{
+	StaticArray<i32, 4> testArray = {0, 1, 2, 3};
+
+	REQUIRE(testArray.front() == 0);
+	REQUIRE(testArray.back() == 3);
+
+	// Access by square brackets is already implicitly tested in the other test cases
+}
+
+TEST_CASE("StaticArray Compile Time Asserts")
+{
+	StaticArray<i32, 4> testArray = {1, 2, 3, 4};
+	static_assert(testArray.size == 4);
 }
