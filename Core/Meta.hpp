495a2130acde694776949fa0528881c6e8ed2fa1
André Lourenço <alourenco.dev@gmail.com>
2021-04-13 20:59:47 +0200

    Provide a meta function to check if a type is copy assignable

--- Core/Meta.hpp
+++ Core/Meta.hpp
@@ -8,9 +8,9 @@ namespace meta
 template<typename t_a, t_a t_value>
 struct vConst { constexpr static t_a value = t_value; };
 
+
 using vTrue = vConst<bool, true>;
 using vFalse = vConst<bool, false>;
-
 
 
 template<typename t_a, typename t_b>
@@ -20,11 +20,22 @@ template<typename t_a>
 struct isSame<t_a, t_a> : vTrue {};
 
 
-
 template<typename t_a, typename ...>
 struct areSame : vTrue {};
 
 template<typename t_a, typename t_b, typename... t_others>
 struct areSame<t_a, t_b, t_others...> : vConst<bool, isSame<t_a, t_b>::value && areSame<t_a, t_others...>::value> {};
 
+
+template<typename t_a, typename t_b, typename = void>
+struct isAssignable : vFalse {};
+
+template<typename t_a, typename t_b>
+struct isAssignable<t_a, t_b, decltype(std::declval<t_a>() = std::declval<t_b>(), void())> : vTrue {};
+
+
+template<typename t_a>
+struct isCopyAssignable : isAssignable<t_a, const t_a &> {};
+
 }
+
--- Core_Tests/Meta_Tests.cpp
+++ Core_Tests/Meta_Tests.cpp
@@ -7,12 +7,14 @@ using namespace meta;
 
 constexpr static char k_tag[] = "[Meta]";
 
+
 TEST_CASE("Templated constants", k_tag)
 {
 	REQUIRE(vConst<int, 1>::value == 1);
 	REQUIRE(vTrue::value);
 	REQUIRE_FALSE(vFalse::value);
 }
+
 
 TEST_CASE("Templated Type comparison", k_tag)
 {
@@ -21,4 +23,25 @@ TEST_CASE("Templated Type comparison", k_tag)
 
 	REQUIRE(areSame<int, int, int, int>::value);
 	REQUIRE_FALSE(areSame<int, char, float, int>::value);
+}
+
+
+class NoCopyOperator
+{
+public:
+	NoCopyOperator() = default;
+	NoCopyOperator &operator = (const NoCopyOperator &) = delete;
+};
+
+class HasCopyOperator
+{
+public:
+	HasCopyOperator() = default;
+};
+
+
+TEST_CASE("Check if type has Copy Operator", k_tag)
+{
+	REQUIRE_FALSE(isCopyAssignable<NoCopyOperator>::value);
+	REQUIRE(isCopyAssignable<HasCopyOperator>::value);
 }
