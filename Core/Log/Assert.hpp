7d66a8768ed6eda40ae5a2a95dacc288f184bed9
André Lourenço <alourenco.dev@gmail.com>
2021-05-26 19:05:59 +0200

    Fix assert compilation

--- Core/Log/Assert.hpp
+++ Core/Log/Assert.hpp
@@ -11,7 +11,7 @@ static const LogSetting k_assertLogSetting = {k_yellowColor, 'A', false};
 };
 
 #if defined(_RELEASE_SYMB)
-#	define g_assert(exp, ...) if (!(exp)) { age::logger::rawLog(age::logger::k_assertLogSetting, __FILE__, __LINE__, "Assert", __VA_ARGS__); _breakpoint(); }
+#	define g_assert(exp, ...) if (!(exp)) { logger::rawLog(logger::k_assertLogSetting, __FILE__, __LINE__, "Assert", __VA_ARGS__); _breakpoint(); }
 #	define g_assertFatal(exp, ...) if (!(exp)) { g_error("Assert Fatal", __VA_ARGS__); }
 #else
 #	define g_assert(exp, ...) ;
