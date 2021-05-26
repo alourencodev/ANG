93e2fd0f6b3ba4581662c85fa5ff3e60c60f477b
André Lourenço <alourenco.dev@gmail.com>
2021-05-26 19:06:49 +0200

    Make bitfield operations easier and more readable
    Create a BitField class that handles usual bit field operations in an easier and more readable way.

---
+++ Core/BitField.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Core/Log/Assert.hpp"
+#include "Core/Types.hpp"
+
+#define g_assertBitIndex(bitIndex) g_assertFatal(bitIndex < 8, "Bit index must be smaller than 8.")
+
+class BitField
+{
+public:
+	BitField() = default;
+	BitField(byte field) : _field(field) {}
+	BitField(const BitField &other) : _field(other._field) {}
+
+	void setAll() { _field = 0xFF; }
+	void clearAll() { _field = 0x00; }
+
+	void set(u8 bitIndex) { g_assertBitIndex(bitIndex); _field |= 1 << bitIndex; }
+	void clear(u8 bitIndex) { g_assertBitIndex(bitIndex); _field &= ~(1 << bitIndex); }
+	void toggle(u8 bitIndex) { g_assertBitIndex(bitIndex); _field ^= 1 << bitIndex; }
+
+	bool isSet(u8 bitIndex) const { g_assertBitIndex(bitIndex); return (_field & (1 << bitIndex)) > 0; }
+	bool isSet(BitField other) const { return (_field & other._field) == _field; }
+	bool isClear() const { return _field == 0; }
+	
+	BitField operator & (BitField other) const { return BitField(_field & other._field); }
+	void operator &= (BitField other) { _field &= other._field; }
+
+	BitField operator | (BitField other) const { return BitField(_field | other._field); }
+	void operator |= (BitField other) { _field |= other._field; }
+
+	BitField operator ^ (BitField other) const { return BitField(_field ^ other._field); }
+	void operator ^= (BitField other) { _field ^= other._field; }
+
+	bool operator == (BitField other) { return _field == other._field; }
+	bool operator == (byte field) { return _field == field; }
+
+	BitField operator << (i32 offset) { return BitField(_field << offset); }
+	BitField operator >> (i32 offset) { return BitField(_field >> offset); }
+	BitField operator ~ () const { return BitField(~_field); }
+
+	operator byte () const { return _field; }
+
+private:
+	byte _field = 0x00;
+};
+
+#undef g_assertBitIndex
---
+++ Core/Tests/BitField_Tests.cpp
@@ -0,0 +1,41 @@
+#include <catch.hpp>
+
+#include <Core/BitField.hpp>
+
+
+constexpr static char k_tag[] = "[BitField]";
+
+
+TEST_CASE("BitField Test", k_tag)
+{
+	BitField bitfield;
+
+	REQUIRE(bitfield.isClear());
+
+	SECTION("Bit manipulation")
+	{
+		// Set to 0000 0010
+		bitfield.set(1);
+		REQUIRE(bitfield == static_cast<byte>(0x02));
+
+		// Set to 0000 0110
+		bitfield.set(2);
+		REQUIRE(bitfield == static_cast<byte>(0x06));
+
+		REQUIRE(bitfield.isSet(1));
+		REQUIRE(bitfield.isSet(2));
+		REQUIRE(bitfield.isSet(BitField(0x06)));
+		REQUIRE_FALSE(bitfield.isSet(BitField(0x02)));
+
+		// Set to 0000 0100
+		bitfield.clear(1);
+		REQUIRE(bitfield.isSet(2));
+		REQUIRE_FALSE(bitfield.isSet(1));
+		REQUIRE(bitfield == static_cast<byte>(0x04));
+
+		// Set to 0000 0001
+		bitfield.toggle(2);
+		bitfield.toggle(0);
+		REQUIRE(bitfield == static_cast<byte>(0x01));
+	}
+}
