bd7fbf0df0b5fdd7d71298435b584d8ef08a5666
André Lourenço <alourenco.dev@gmail.com>
2021-06-28 21:52:40 +0200

    Test HashMap Constructors

---
+++ Core/Tests/HashMap_Tests.cpp
@@ -0,0 +1,55 @@
+#include <catch.hpp>
+
+#include <string>
+
+#include <Core/HashMap.hpp>
+
+constexpr static char k_tag[] = "[HashMap]";
+
+TEST_CASE("Hash Map Constructor", k_tag)
+{
+	SECTION("Default Constructor")
+	{
+		HashMap<std::string, int> map;
+		
+		REQUIRE(map.capacity() == 0);
+		REQUIRE(map.count() == 0);
+		REQUIRE(map.isEmpty());
+	}
+
+	SECTION("Capacity Constructor")
+	{
+		HashMap<std::string, int> map(4);
+		
+		REQUIRE(map.capacity() == 8);
+		REQUIRE(map.count() == 0);
+		REQUIRE(map.isEmpty());
+
+		HashMap<std::string, int> map2(10);
+		REQUIRE(map2.capacity() == 16);
+		REQUIRE(map2.count() == 0);
+		REQUIRE(map2.isEmpty());
+	}
+
+	SECTION("Initializer List Constructor")
+	{
+		HashMap<std::string, int> map = {{"aba", 1}, {"baba", 1}, {"other", 3}};
+
+		REQUIRE(map.capacity() == 8);
+		REQUIRE(map.count() == 3);
+		REQUIRE_FALSE(map.isEmpty());
+
+		REQUIRE(map["aba"] == 1);
+		REQUIRE(map["baba"] == 1);
+		REQUIRE(map["other"] == 3);
+
+		REQUIRE(map.contains("aba"));
+		REQUIRE_FALSE(map.contains("1337"));
+	}
+}
+
+
+TEST_CASE("HashMap Resize", k_tag)
+{
+	// TODO
+}
