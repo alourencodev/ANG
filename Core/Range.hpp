acb63036dc49e9a5f67e51db9f61c2743a0d72c7
André Lourenço <alourenco.dev@gmail.com>
2021-07-05 19:52:15 +0200

    Add Range struct

---
+++ Core/Range.hpp
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+
+#include "Core/Attributes.hpp"
+#include "Core/Log/Assert.hpp"
+#include "Core/Meta.hpp"
+#include "Core/Types.hpp"
+
+template<typename t_type>
+class Range
+{
+public:
+	using Iterator = t_type *;
+	using ConstIterator = const t_type *;
+
+	Range(t_type *data, size_t count) : _data(data), _count(count) {}
+	Range(const Range &) = delete;
+	Range(Range &&) = delete;
+
+	_force_inline const t_type &operator[](u32 index) const
+	{
+		g_assertFatal(index < _count, "Range out of bounds! Trying to access index %d of Range with size %d", index, _count);
+		return _data[index];
+	}
+
+	_force_inline t_type &operator[](u32 index)
+	{
+		g_assertFatal(index < _count, "Range out of bounds! Trying to access index %d of Range with size %d", index, _count);
+		return _data[index];
+	}
+
+	_force_inline const t_type *data() const { return _data; }
+	_force_inline const size_t count() const { return _count; }
+	_force_inline size_t lastIndex() const { return _count - 1; }
+
+	_force_inline explicit operator const t_type *() const { return _data; }
+	_force_inline explicit operator t_type *() { return _data; }
+
+	Iterator begin() { return _data; }
+	ConstIterator begin() const { return _data; }
+	Iterator end() { return _data + lastIndex(); }
+	ConstIterator end() const { return _data + lastIndex(); }
+
+	t_type &front() { return _data[0]; }
+	const t_type &front() const { return _data[0]; }
+	t_type &back() { return _data[lastIndex()]; }
+	const t_type &back() const { return _data[lastIndex()]; }
+
+	void fill(const t_type &value) { std::fill_n(_data, _count, value); }
+
+	const t_type *find(const t_type &value) const
+	{
+		static_assert(meta::isEqualComparable<t_type>::value, "Can't call find if Range type is not equal comparable.");
+		for (u32 i = 0; i < _count; i++) {
+			if (_data[i] == value)
+				return &_data[i];
+		}
+
+		return end();
+	}
+
+	t_type *find(const t_type &value)
+	{
+		static_assert(meta::isEqualComparable<t_type>::value, "Can't call find if Range type is not equal comparable.");
+		for (u32 i = 0; i < _count; i++) {
+			if (_data[i] == value)
+				return &_data[i];
+		}
+
+		return end();
+	}
+
+	const t_type *findBackwards(const t_type &value) const
+	{
+		static_assert(meta::isEqualComparable<t_type>::value, "Can't call find if Range type is not equal comparable.");
+		for (i32 i = static_cast<i32>(lastIndex()); i >= 0; i--) {
+			if (_data[i] == value) 
+				return &_data[i];
+		}
+
+		return end();
+	}
+
+	t_type *findBackwards(const t_type &value)
+	{
+		static_assert(meta::isEqualComparable<t_type>::value, "Can't call find if Range type is not equal comparable.");
+		for (i32 i = static_cast<i32>(lastIndex()); i >= 0; i--) {
+			if (_data[i] == value) 
+				return &_data[i];
+		}
+
+		return end();
+	}
+
+	bool contains(const t_type &value) const { return find(value) != end(); }
+	bool containsBackwards(const t_type &value) const { return findBackwards(value) != end(); }
+
+private:
+	t_type * const _data = nullptr;
+	const size_t _count = 0;
+};
+
+template<typename t_type>
+static std::ostream &operator<<(std::ostream &os, const Range<t_type> &range)
+{
+	os << "[";
+	for (u32 i = 0; i < range.lastIndex(); i++)
+		os << range[i] << ", ";
+
+	os << range.back() << "]";
+
+	return os;
+}
+
+template<typename t_type>
+static std::istream &operator>>(std::istream &is, Range<t_type> &range)
+{
+	for (u32 i = 0; i < range.count(); i++)
+		is >> range[i];
+
+	return is;
+}
---
+++ Core/Tests/Range_Tests.cpp
@@ -0,0 +1,87 @@
+#include <catch.hpp>
+
+#include <sstream>
+
+#include <Core/Range.hpp>
+
+constexpr static char k_tag[] = "[Range]";
+
+TEST_CASE("Range Constructor", k_tag)
+{
+	const int a[4] = {1, 2, 3, 4};
+	const Range range(a, 4);
+
+	REQUIRE(range[0] == a[0]);
+	REQUIRE(range[1] == a[1]);
+	REQUIRE(range[2] == a[2]);
+	REQUIRE(range[3] == a[3]);
+
+	REQUIRE(range.lastIndex() == 3);
+}
+
+TEST_CASE("Range Iteration", k_tag)
+{
+	const int a[4] = {1, 2, 3, 4};
+	const Range range(a, 4);
+
+	for(int i : range) 
+		REQUIRE(range[i] == (i+1));
+}
+
+TEST_CASE("Range Query", k_tag)
+{
+	const int a[4] = {1, 2, 3, 4};
+	const Range range(a, 4);
+
+	SECTION("Find elements")
+	{
+		REQUIRE(range.find(2) == &range[1]);
+		REQUIRE(range.find(1) == &range[0]);
+		REQUIRE(range.find(4) == &range[3]);
+		REQUIRE(range.find(8) == range.end());
+
+		REQUIRE(range.findBackwards(2) == &range[1]);
+		REQUIRE(range.findBackwards(1) == &range[0]);
+		REQUIRE(range.findBackwards(4) == &range[3]);
+		REQUIRE(range.findBackwards(8) == range.end());
+	}
+
+	SECTION("Contains elements")
+	{
+		REQUIRE(range.contains(2));
+		REQUIRE(range.contains(1));
+		REQUIRE(range.contains(3));
+		REQUIRE_FALSE(range.contains(8));
+
+		REQUIRE(range.containsBackwards(2));
+		REQUIRE(range.containsBackwards(1));
+		REQUIRE(range.containsBackwards(3));
+		REQUIRE_FALSE(range.containsBackwards(8));
+	}
+}
+
+TEST_CASE("Range Stream Operators", k_tag)
+{
+	SECTION("Stream In")
+	{
+		int a[4] = {0, 0, 0, 0};
+		Range range(a, 4);
+
+		std::istringstream is("1 2 3 4");
+		is >> range;
+
+		for (int i = 0; i < range.count(); i++)
+			REQUIRE(range[i] == i+1);
+	}
+
+	SECTION("Stream Out")
+	{
+		const int a[4] = {1, 2, 3, 4};
+		const Range range(a, 4);
+
+		std::stringstream ss;
+		ss << range;
+
+		REQUIRE(ss.str() == "[1, 2, 3, 4]");
+	}
+}
