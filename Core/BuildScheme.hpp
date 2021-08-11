2a2ea30ff50c2fa3c3900d219e3e928acbea213f
André Lourenço <alourenco.dev@gmail.com>
2021-08-11 12:26:28 +0200

    Clean BuildScheme
    - Remove remains of the cmake era.

--- Core/BuildScheme.hpp
+++ Core/BuildScheme.hpp
@@ -1,33 +1,19 @@
 #pragma once
 
 #ifdef AGE_UNIT_TEST
-#	define AGE_DEBUG
+#	ifndef AGE_DEBUG
+#		define AGE_DEBUG
+#	endif
 #	define IF_TEST(X) X
 #else
 #	define IF_TEST(X)
 #endif
 
-#if _MSC_VER
-#   ifdef AGE_CMAKE_Debug
-#   	ifndef AGE_DEBUG
-#   		define AGE_DEBUG
-#   	endif
-#   endif
-#endif
-
 #ifdef AGE_DEBUG
 #	define IF_DEBUG(X) X
-#	define AGE_RELEASE_DBG_INFO 1
+#	define AGE_RELEASE_DBG_INFO
 #else
 #	define IF_DEBUG(X)
-#endif
-
-#if _MSC_VER
-#   ifdef AGE_CMAKE_ReleaseDbgInfo
-#   	ifndef AGE_RELEASE_DBG_INFO
-#   		define AGE_RELEASE_DBG_INFO
-#   	endif
-#   endif
 #endif
 
 #ifdef AGE_RELEASE_DBG_INFO
