f1e1b4d5131e0fd847e2bdfc3b10c880164ca950
André Lourenço <alourenco.dev@gmail.com>
2021-12-03 22:40:27 +0100

    Allow to have a Pair has a HashMap key

---
+++ Core/Pair.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <Core/HashMap.hpp>
+
+#include <algorithm>
+
+namespace age
+{
+
+template<typename t_first, typename t_second>
+struct Pair
+{
+	t_first first;
+	t_second second;
+
+	bool operator == (const Pair &other) const { return first == other.first && second == other.second; }
+	bool operator != (const Pair &other) const { return first != other.first || second != other.second; }
+};
+
+namespace
+{
+
+template<typename t_first, typename t_second>
+class PairHashMapBehavior
+{
+public:
+	bool isEqual(const Pair<t_first, t_second> &a, const Pair<t_first, t_second> &b) const { return a == b; }
+
+	size_t hash(const Pair<t_first, t_second> &value) const 
+	{ 
+		return std::hash<t_first>(value.first) ^ std::hash<t_second>(value.second); 
+	}
+};
+
+}
+
+
+
+template<typename t_first, typename t_second, typename t_valueType, typename t_allocator = DefaultHashMapAllocator<Pair<t_first, t_second>, t_valueType>>
+using PairMap = HashMap<Pair<t_first, t_second>, t_valueType, PairHashMapBehavior, t_allocator>;
+
+}
