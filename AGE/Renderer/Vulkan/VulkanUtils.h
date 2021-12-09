de37ee3e7d5f1df55f8e7380538dceae73a9acc9
André Lourenço <alourenco.dev@gmail.com>
2021-12-09 21:20:24 +0100

    Prevent Vulkan Checks in Release

--- AGE/Renderer/Vulkan/VulkanUtils.h
+++ AGE/Renderer/Vulkan/VulkanUtils.h
@@ -22,11 +22,15 @@ VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBits
 
 #define age_vulkanError(result) logVulkanError(__FILE__, __LINE__, result)
 
+#ifdef AGE_RELEASE
+#define AGE_VK_CHECK(exp) exp
+#else
 #define AGE_VK_CHECK(exp)																												\
 {																																		\
 	VkResult vkCheckResult = exp;																										\
 	if (vkCheckResult != VK_SUCCESS)																									\
 		age_error("Vulkan", "Vulkan check failed for expression (" #exp ") with error %s", parseVulkanError(vkCheckResult).c_str());	\
 }
+#endif
 
 #define AGE_VK_GET_COMMAND(instance, command_name) reinterpret_cast<PFN_##command_name>(vkGetInstanceProcAddr(instance, #command_name))
