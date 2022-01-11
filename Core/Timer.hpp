6e1cf5b35866ae98322b7f37b797e9f748389eeb
André Lourenço <alourenco.dev@gmail.com>
2022-01-12 00:01:01 +0100

    Add simple timer code

---
+++ Core/Timer.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <chrono>
+
+#include <Core/Types.hpp>
+
+
+namespace age
+{
+
+class Timer
+{
+public:
+	void start() { mStartTimePoint = Clock::now(); };
+	u64 millis() const { return std::chrono::duration_cast<Millis>(Clock::now() - mStartTimePoint).count(); };
+	u64 secs() const { return std::chrono::duration_cast<Secs>(Clock::now() - mStartTimePoint).count(); };
+
+private:
+	using Clock = std::chrono::system_clock;
+	using TimePoint = std::chrono::time_point<Clock>;
+	using Millis = std::chrono::milliseconds;
+	using Secs = std::chrono::seconds;
+	
+	TimePoint mStartTimePoint = TimePoint::min();
+};
+
+}	// namespace age
+
+
