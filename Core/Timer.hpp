d3bbaeb882a3cf9f2f23a5311b34cb354105279c
André Lourenço <alourenco.dev@gmail.com>
2022-03-26 02:51:18 +0100

    Improve Timer precision - Use steady clock instead of system clock. This means we now use a monotonic clock, which is the appropriate one for the TImer, since it is expected to be used for time intervals. - Add support for microseconds.

--- Core/Timer.hpp
+++ Core/Timer.hpp
@@ -12,12 +12,14 @@ class Timer
 {
 public:
 	void start() { mStartTimePoint = Clock::now(); };
+	u64 micros() const { return std::chrono::duration_cast<Micros>(Clock::now() - mStartTimePoint).count(); }
 	u64 millis() const { return std::chrono::duration_cast<Millis>(Clock::now() - mStartTimePoint).count(); };
 	u64 secs() const { return std::chrono::duration_cast<Secs>(Clock::now() - mStartTimePoint).count(); };
 
 private:
-	using Clock = std::chrono::system_clock;
+	using Clock = std::chrono::steady_clock;
 	using TimePoint = std::chrono::time_point<Clock>;
+	using Micros = std::chrono::microseconds;
 	using Millis = std::chrono::milliseconds;
 	using Secs = std::chrono::seconds;
 	
