127977e8c1120da99e0e15579078fbd099f29ef0
André Lourenço <alourenco.dev@gmail.com>
2022-05-30 22:04:32 +0200

    Support attributes for g++

--- Core/Attributes.hpp
+++ Core/Attributes.hpp
@@ -1,15 +1,23 @@
 #pragma once
 
-#ifdef _MSC_VER
-
-#include <intrin.h>
-
-#define _force_inline __forceinline
-#define _breakpoint() __debugbreak()
+// Compiler Attributes
+#if defined(_MSC_VER)
+#	include <intrin.h>
+#	define _force_inline __forceinline
+#elif defined(__GNUC__)
+#	define _force_inline __attribute__((always_inline))
 #else
-#warning ("Attributes are only available for MSVC.")
-#define _force_inline
-#define _breakpoint()
+#	warning ("Attributes are only available for MSVC.")
+#	define _force_inline
 #endif
+
+
+// Dev Environment Attributes
+#ifdef _MSC_VER
+#	define _breakpoint() __debugbreak()
+#else
+#	define _breakpoint()
+#endif
+
 
 #define _nodiscard [[nodiscard]]
