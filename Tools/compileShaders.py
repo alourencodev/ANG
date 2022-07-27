0f115be260f9fd02fdb29550167bdcf0be83f465
André Lourenço <alourenco.dev@gmail.com>
2022-07-27 20:19:10 +0200

    Support shader compilation from recursive directories

--- Tools/compileShaders.py
+++ Tools/compileShaders.py
@@ -2,8 +2,11 @@ import argparse
 import os
 import subprocess
 
-# tools
+# Tools
 compiler = "glslc"
+
+# Settings
+supported_extensions = [".vert", ".frag"]
 
 # Setup arguments
 parser = argparse.ArgumentParser()
@@ -16,74 +19,96 @@ args = parser.parse_args()
 
 
 def log(str, force_log = False):
-	global args
-	if args.verbose or force_log:
-		print("[ShaderCompilation]: " + str)
+    global args
+    if args.verbose or force_log:
+        print("[ShaderCompilation]: " + str)
 
 
-def compile_shader(shader_file_name, include_dirs):
-	global args
+def compile_shader(shader_dir, include_dirs):
+    global args
 
-	shader_dir = args.source + shader_file_name
-	output_shader_dir = args.output + shader_file_name + ".spv";
-	compile_command = [compiler, shader_dir, "-o", output_shader_dir]
+    shader_file_name = os.path.basename(shader_dir)
+    output_shader_dir = args.output + shader_file_name + ".spv";
+    compile_command = [compiler, shader_dir, "-o", output_shader_dir]
 
-	if args.optimize:
-		compile_command.append("-O")
+    if args.optimize:
+        compile_command.append("-O")
 
-	if include_dirs:
-		compile_command = compile_command + include_dirs
+    if include_dirs:
+        compile_command = compile_command + include_dirs
 
-	log("Compiling " + shader_dir)
+    log("Compiling " + shader_dir)
 
-	result = subprocess.run(compile_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	error = result.stderr.decode('utf-8')
-	if error:
-		log("Error running " + str(compile_command))
-		log(error.replace('\n', ''))
-		return False
-	else:
-		stdOutput = result.stdout.decode('utf-8')
-		if stdOutput:
-			log(stdOutput.replace('\n',''))
+    result = subprocess.run(compile_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    error = result.stderr.decode('utf-8')
+    if error:
+        log("Error running " + str(compile_command))
+        log(error.replace('\n', ''))
+        return False
+    else:
+        stdOutput = result.stdout.decode('utf-8')
+        if stdOutput:
+            log(stdOutput.replace('\n',''))
 
-	return True
+    return True
+
+
+def list_shaders(source_dir):
+    shaders = []
+    dirs = []
+
+    # Split shaders from directories.
+    paths = os.listdir(source_dir)
+    for path in paths:
+        path_dir = source_dir + path
+        if os.path.isfile(path_dir):
+            extension = os.path.splitext(path)[-1]
+            if extension in supported_extensions:
+                shaders.append(path_dir)
+        else:
+            log("dir: " + path)
+            dirs.append(path_dir + "\\")
+
+    for dir in dirs:
+        shaders = shaders + list_shaders(dir)
+        
+    return shaders
 
 
 def main():
-	global args
+    global args
 
-	log("Compiling shaders from path " + args.source + " to destination " + args.output)
+    log("Compiling shaders from path " + args.source + " to destination " + args.output)
 
-	if args.optimize:
-		log("Optimize enabled.")
+    if args.optimize:
+        log("Optimize enabled.")
 
-	# Build list of include Directories
-	include_dirs = []
-	if not args.includes is None:
-		log("Include Directories:")
-		for include_dir in args.includes:
-			log(" - " + include_dir)
-			include_dirs = include_dirs + ["-I", include_dir]
+    # Build list of include Directories
+    include_dirs = []
+    if not args.includes is None:
+        log("Include Directories:")
+        for include_dir in args.includes:
+            log(" - " + include_dir)
+            include_dirs = include_dirs + ["-I", include_dir]
 
-	# Create output directory if necessary
-	if not os.path.exists(args.output):
-		log("Creating " + args.output)
-		os.system("mkdir " + args.output)
+    # Create output directory if necessary
+    if not os.path.exists(args.output):
+        log("Creating " + args.output)
+        os.system("mkdir " + args.output)
 
-	# Compile shaders
-	for file_name in os.listdir(args.source):
-		extension = os.path.splitext(file_name)[-1];
+    # Compile shaders
+    shaders = list_shaders(args.source)
+    log("Listed Shaders: " + str(shaders))
 
-		if (extension == ".vert") or (extension == ".frag"):
-			result = compile_shader(file_name, include_dirs);
+    for shader in shaders:
+        result = compile_shader(shader, include_dirs);
 
-			if not result:
-				log("Compilation Failed")
-				return
+        if not result:
+            log("Compilation Failed")
+            return
 
-	log("Compilation Complete", True)
+    log("Compilation Complete", True)
 
 
 if __name__ == '__main__':
-	main()
+    main()
